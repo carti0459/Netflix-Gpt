f1d941bc0fa1dcb1f614eba773edd83c8835fc0f
Karthik <karthiknair2004@gmail.com>
2025-06-30 01:02:28 +0530

    Firebase Setup,hosting,sign in , sign up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,54 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validation";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    //Validating the Form data
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (message) return;
+
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //Sign in Logic
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage)
+        });
+    }
+  };
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -16,32 +62,44 @@ const Login = () => {
           alt="logo"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black opacity-80 my-36 mx-auto right-0 left-0 text-white rounded-lg ">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black opacity-80 my-36 mx-auto right-0 left-0 text-white rounded-lg "
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "SignUp"}
         </h1>
-        {!isSignInForm && <input
-          type="text"
-          placeholder="Full Name"
-          className="p-4 my-4 w-full bg-gray-800 rounded-lg"
-        />
-        }
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-800 rounded-lg"
+          />
+        )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-800 rounded-lg"
         />
-        
+
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-800 rounded-lg"
         />
-        <button className="p-4 my-6 bg-red-600 w-full rounded-lg cursor-pointer">
+        <p className="text-red-500 text-lg font-bold py-2">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-600 w-full rounded-lg cursor-pointer"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "SignUp"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-          {isSignInForm ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now"}
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now"}
         </p>
       </form>
     </div>
