e94f7003eaeca2cdd86e60cd640c7f00e5fdf968
Karthik <karthiknair2004@gmail.com>
2025-07-06 23:56:24 +0530

    TMDB Video API Setup

---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
+const VideoBackground = ({ movieId }) => {
+  const trailerView = useSelector(store => store.movies?.trailerVideo);
+
+  const dispatch = useDispatch();
+  //Fetching Movie trailer
+
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/541671/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerView?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </div>
+  );
+};
+
+export default VideoBackground;
